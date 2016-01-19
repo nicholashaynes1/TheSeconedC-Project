3fea9ee6cc222e51d15b48f7a04a97163df6b9a0
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-01-19 12:42:09 -0700

    Added the header file.

---
+++ src/controller/SeconedC++Controller.h
@@ -0,0 +1,24 @@
+/*
+ * SeconedC++Controller.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: nhay7834
+ */
+
+#ifndef CONTROLLER_SECONEDC__CONTROLLER_H_
+#define CONTROLLER_SECONEDC__CONTROLLER_H_
+
+class SeconedCController
+{
+private:
+	int classCount;
+public:
+	SeconedCController();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_SECONEDC__CONTROLLER_H_ */
